86f9915dfe371fc128b82d9a066233125762239a
jliendro <jose.liendro@gmail.com>
2018-08-20 12:14:55 -0300

    debug false on production

--- control_ofelia/settings.py
+++ control_ofelia/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#pkyiy3rvx7n%nwlpgyyq+09lq9!i*8q5^a5)0e75j+b7d37z1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['control-ofelia.herokuapp.com', 'localhost']
 
